e8c044212833923d4f33d0926424e2f102757c33
karteekkristipati8123 <karthikkristipati123@gmail.com>
2023-10-18 11:42:50 +0530

    Tuple

---
+++ demo.AccessingFrontandRear.py
@@ -0,0 +1,8 @@
+# Accessing front and rear elements 
+Tuple1 = (10,50,30,70) 
+
+front = Tuple1[0]
+rear = Tuple1[-1]
+res = (front, rear)
+
+print(res)
---
+++ demo.CreationofTuples.py
@@ -0,0 +1,13 @@
+# Creating a tuple with the use of string
+Tuple1=('Karthik','Reddy')
+print("\n Tuple with the use of string:")
+print(Tuple1)
+
+# Creating a tuple with the user of list 
+list1=[1,2,3,4,5]
+print("\n tuple with the use of list:")
+print(tuple(list1))
+
+Tuple1=tuple('Reddy')
+print("\n Tuple with the function:")
+print(Tuple1)
